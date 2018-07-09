f2c49d3ce56a2be674a0cd5e51f081433e84729b
mchardex <bukunmiadebisi@gmail.com>
2018-07-09 18:06:08 +0100

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyB7yd0HQh6FRNFX-um2VCY9H__NPpr0NZA",
+    authDomain: "react-notes-c5c18.firebaseapp.com",
+    databaseURL: "https://react-notes-c5c18.firebaseio.com",
+    projectId: "react-notes-c5c18",
+    storageBucket: "react-notes-c5c18.appspot.com",
+    messagingSenderId: "395771177656"
+  };
